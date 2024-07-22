53134a76250a85a4cde4895d57957c759051b775
KarmaAndEmptiness <wupeiling@tutamail.com>
2024-07-22 13:20:17 +0000

    2024.7.22 update

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,7 +2,7 @@
     <div class="about">
         <div class="m-title pt-4 pb-5 mb-2">关于</div>
         <div>
-            github : <a href="https://github.com/Woo0ow">https://github.com/Woo0ow</a>
+            github : <a target="_blank" href="https://github.com/KarmaAndEmptiness">https://github.com/KarmaAndEmptiness</a>
         </div>
     </div>
 </template>
