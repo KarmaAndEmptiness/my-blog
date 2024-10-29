fdcf4cd8977cb9aa27e0ee59a5815a9f222b55a5
KarmaAndEmptiness <3329277943@qq.com>
2024-10-29 13:41:21 +0000

    2024.10.29 update

--- mock/index.js
+++ mock/index.js
@@ -1,9 +1,10 @@
 import article from './modules/article'
 import tag from './modules/tag'
 import archive from './modules/archive'
-Mock.mock('api/articles','get',article)
-Mock.mock(/api\/articles\/\d+/,'post',article)
-Mock.mock('api/tags','get',tag)
-Mock.mock(/api\/tags\/\d+/,'post',tag)
-Mock.mock('api/archive','get',archive)
+import Mock from 'mockjs'
+Mock.mock('api/articles', 'get', article)
+Mock.mock(/api\/articles\/\d+/, 'post', article)
+Mock.mock('api/tags', 'get', tag)
+Mock.mock(/api\/tags\/\d+/, 'post', tag)
+Mock.mock('api/archive', 'get', archive)
 
