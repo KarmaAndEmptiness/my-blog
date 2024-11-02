b57aa366297eb1c77f66396741e95e20b592d30c
KarmaAndEmptiness <3329277943@qq.com>
2024-11-02 12:38:20 +0000

    2024.11.2 update

--- src/views/Links.vue
+++ src/views/Links.vue
@@ -1,10 +1,21 @@
 <template>
     <div class="links">
         <div class="m-title pt-4 pb-5 mb-2">友链</div>
+        <div>
+            Discrete Mathematics:<a target="_blank" href="../books/discrete-mathematics.pdf">Richard Johnsonbaugh - Discrete
+                Mathematics-Pearson (2017)</a>
+        </div>
+        <div>
+            Linear Algebra and Its
+            Applications :<a target="_blank" href="../books/linear-algebra-and-its-applications-4th-edition.pdf">David C.
+                Lay - Linear Algebra and Its
+                Applications - 4th Edition</a>
+        </div>
+
     </div>
 </template>
 <script>
-    export default{
-        name:'Links'
-    }
+export default {
+    name: 'Links'
+}
 </script>
